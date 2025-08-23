9a4d117250ab073dfe7c3ce09e197698c28c7d76
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-08-23 15:46:46 +0600

    added bank section

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 
 document.getElementById("loginButton").addEventListener("click", function (e) {
   e.preventDefault();
-  const mobileNumber = 01234567890;
+  const mobileNumber = "01234567890";
+  const myNumber = parseInt(mobileNumber);
   const pinNumber = 1234;
   const mobileNumberValue = document.getElementById("mobile-number").value;
   const mobileNumberValueConvert = parseInt(mobileNumberValue);
   const pinNumberValue = document.getElementById("pin-number").value;
   const pinNumberValueConvert = parseInt(pinNumberValue);
   if (
-    mobileNumberValueConvert === mobileNumber &&
+    mobileNumberValueConvert === myNumber &&
     pinNumberValueConvert === pinNumber
   ) {
     window.location.href = "./home.html";
